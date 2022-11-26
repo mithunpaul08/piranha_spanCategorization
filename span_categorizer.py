1bb3d3f2ab12e3590052dd9e292fdcd74f0b9f85
Mitch Mithun <mitch@D21ML-MMITHUN.lan>
2022-11-25 21:24:07 -0800

    this version of code works/was used to train a pipelien of spancategorize and tok2vec. accurices are absymal but works without any memory or segfaults

--- span_categorizer.py
+++ span_categorizer.py
@@ -1,5 +1,7 @@
 import spacy
 nlp=spacy.load('en_core_web_trf')
-doc=nlp("this is a sentence")
-for token in doc:
-    print(token.text)
+
+texts=["what is this","Washington said that","Mark Zuckerberg is the owner of Meta"]
+
+for doc in nlp.pipe(texts):
+    print([(ent.text, ent.label_) for ent in doc.ents])
